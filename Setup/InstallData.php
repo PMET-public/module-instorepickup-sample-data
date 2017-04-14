3893fbd9c1e80ba48d565b157681657f6d755e5b
Jeffrey Britts <jbritts@magento.com>
2017-04-14 11:14:51 -0400

    converted to importer

--- Setup/InstallData.php
+++ Setup/InstallData.php
@@ -1,62 +1,35 @@
 <?php
-
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
 namespace MagentoEse\InStorePickupSampleData\Setup;
 
-use Magento\Catalog\Model\ProductFactory;
-use Magento\Framework\Setup\InstallDataInterface;
-use Magento\Framework\Setup\ModuleContextInterface;
-use Magento\Framework\Setup\ModuleDataSetupInterface;
-use Magento\Framework\App\State;
-use Magento\Catalog\Model\Product\Attribute\Repository;
+use Magento\Framework\Setup;
 
-    /**
- * @codeCoverageIgnore
- */
-class InstallData implements InstallDataInterface
+class InstallData implements Setup\InstallDataInterface
 {
     /**
-     * ispuData
-     *
-     * @var array
+     * @var Setup\SampleData\Executor
      */
-    private $ispuData;
-    private $productFactory;
-    private $state;
-    private $productAttributeRepository;
+    protected $executor;
 
+    /**
+     * @var Installer
+     */
+    protected $installer;
 
-    public function __construct(ProductFactory $productFactory, State $state, Repository $productAttributeRepository)
+    public function __construct(Setup\SampleData\Executor $executor, Installer $installer)
     {
-        $this->productFactory = $productFactory;
-        //Area code is required, but cannot be set if already set by another module
-        //Try to set it. If it fails, ignore it.
-        try{
-            $state->setAreaCode('adminhtml');
-        }
-        catch(\Magento\Framework\Exception\LocalizedException $e){
-            // left empty
-        }
-        $this->ispuData = require 'ISPUData.php';
-        $this->productAttributeRepository = $productAttributeRepository;
+        $this->executor = $executor;
+        $this->installer = $installer;
     }
 
-    public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    /**
+     * {@inheritdoc}
+     */
+    public function install(Setup\ModuleDataSetupInterface $setup, Setup\ModuleContextInterface $moduleContext)
     {
-        $_attribcode='in_store_available';
-
-         $_product = $this->productFactory->create();
-        foreach ($this->ispuData as $_pickupData) {
-
-            //get product
-            $_product->load($_product->getIdBySku($_pickupData[0]));
-
-            //get attribute based on text value
-            $_attr = $_product->getResource()->getAttribute( $_attribcode);
-            $_optionId = $_attr->getSource()->getOptionId($_pickupData[1]);
-
-            //Set data and save product
-            $_product->setData($_attribcode,$_optionId );
-            $_product->getResource()->saveAttribute($_product,$_attribcode);
-        }
+        $this->executor->exec($this->installer);
     }
 }
---
+++ Setup/Installer.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace MagentoEse\InStorePickupSampleData\Setup;
+
+use Magento\Framework\Setup;
+
+
+class Installer implements Setup\SampleData\InstallerInterface
+{
+
+   protected $productUpdate;
+
+
+    public function __construct(
+        \MagentoEse\ProductSampleDataUpdate\Model\ProductUpdate $productUpdate
+
+    ) {
+        $this->productUpdate = $productUpdate;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function install()
+    {
+        $this->productUpdate->install(['MagentoEse_InStorePickupSampleData::fixtures/ISPUData.csv']);
+    }
+}
